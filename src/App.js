7915eed6b981f4cc1cf3c04b40f05ff11fa0b1a0
papapin <papapin777#gmail.com>
2018-11-01 21:47:51 -0700

    step14

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ class App extends Component {
 
   }
 
+  changeTitle = () => {
+    this.setState({pageTitle: 'NONE!'})
+  }
+
   render() {
 
     const cars = this.state.cars;
@@ -36,6 +40,7 @@ class App extends Component {
 
     return <div style={divStyle}>
       <h1>{this.state.pageTitle}</h1>
+        <button onClick={this.changeTitle}>change title</button>
         <Car name={cars[0].name} year={cars[0].year} />
         <Car name={cars[1].name} year={cars[1].year} />
         <Car name={cars[2].name} year={cars[2].year} />
