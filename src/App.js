3da4653c5461425d24a3dd0bc6a6ba4eda34099a
papapin <papapin777#gmail.com>
2018-11-01 21:02:44 -0700

    step10

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Car from  './Car/Car';
+
 class App extends Component {
+
+
   render() {
+
+    const divStyle = {
+      'textAlign': 'center'
+    }
+
     return (
-      <div>
+      <div style={divStyle}>
         <h1>hello</h1>
+        <Car/>
       </div>
     );
   }
---
+++ src/Car/Car.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Car extends Component {
+    render () {
+        return (
+            <div>
+                <p>Car!</p>
+            </div>
+        )
+    }
+}
+
+export default Car
