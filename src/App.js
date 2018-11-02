ef8c1c9d3a23d6e29353ff7da1af0deecc299b9d
papapin <papapin777#gmail.com>
2018-11-01 21:42:33 -0700

    step13

--- src/App.js
+++ src/App.js
@@ -5,23 +5,41 @@ import Car from  './Car/Car';
 
 class App extends Component {
 
+  state = {
+
+    cars: [
+      {
+        name: 'Toyota',
+        year: 2016
+      },
+      {
+        name: 'Ford',
+        year: 2018
+      },
+      {
+        name: 'Dodge',
+        year: 2014
+      }
+
+    ],
+    pageTitle: 'React components'
+
+  }
 
   render() {
+
+    const cars = this.state.cars;
 
     const divStyle = {
       'textAlign': 'center'
     }
 
-    return (
-      <div style={divStyle}>
-        <h1>hello</h1>
-        <Car name={'Ford'} year={2018}/>
-        <Car name={'Toyota'} year={2017}/>
-        <Car name={'Test'} year={2017}>
-          <p>!!!!!!!</p>
-        </Car>
-      </div>
-    );
+    return <div style={divStyle}>
+      <h1>{this.state.pageTitle}</h1>
+        <Car name={cars[0].name} year={cars[0].year} />
+        <Car name={cars[1].name} year={cars[1].year} />
+        <Car name={cars[2].name} year={cars[2].year} />
+      </div>;
   }
 }
 
